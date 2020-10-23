a36cd2c4b7558f4bbda295c9b3cddb0467ff24a2
Filipa Simao <fsimao@infraspeak.com>
2020-10-23 19:13:36 +0100

    Fix issues list

--- src/components/IssuesList.vue
+++ src/components/IssuesList.vue
@@ -1,15 +1,17 @@
 <template>
-    <div class="issues">
-        <div class="list" :key="result.repo.name" v-for="result in results">
-            <a :href="result.repo.url">
-                <h1>{{ result.repo.name }} ({{ result.issues.length }})</h1>
+    <div class="list">
+        <div :key="result.repo.name" v-for="result in results">
+            <a :href="result.repo.url" target="_blank">
+                <h1>{{ result.repo.name }}</h1>
             </a>
-            <div v-for="issue in result.issues" :key="issue.name">
+            <div class="issues" v-for="issue in result.issues" :key="issue.name">
                 <div>
                     <a :href="issue.url">
                         <h2>{{ issue.title }}</h2>
                     </a>
-                    <p v-for="tag in issue.tags" :key="tag">{{ tag }}</p>
+                    <p v-for="tag in issue.tags" :key="tag" class="tags">
+                        <span>{{ tag }}</span>
+                    </p>
                 </div>
                 <span>{{ issue.status }}</span>
                 <span>{{ issue.id }} opened {{issue.date_opened}} ago by {{issue.author}}</span>
@@ -21,19 +23,55 @@
 <script lang="ts">
     import { defineComponent } from 'vue'
 
-    interface ComponentData {
-        results: [];
-    }
-
     export default defineComponent({
         name: 'Issues list',
-        data (): ComponentData {
-            return {
-                results: []
-            }
+        props: {
+            results: Array
         }
     })
 </script>
 
 <style lang="scss">
+    .list {
+        background-color: var(--issues);
+        height: 100%;
+        width: 100%;
+        padding: 1rem 1rem;
+
+        > div{
+            border-bottom: 2px solid var(--grey);
+
+            a {
+            text-decoration: none;
+                h1 {
+                    color: var(--dark-blue);
+                    display: inline-block;
+                }
+            }
+            .issues {
+                > div {
+                    .tags {
+                        display: flex;
+
+                        span {
+                            background: var(--tag);
+                            color: var(--button);
+                            border-radius: 20px;
+                            padding-right: 1rem;
+                        }
+                    }
+                }
+
+                span {
+                    &:first-child {
+                        color: var(--black)
+                    }
+
+                    &:last-child {
+                        color: var(--description)
+                    }
+                }
+            }
+        }
+    }
 </style>
